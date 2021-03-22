f9b9a65e5916e6cddff04734611744aee4c45190
FilipAlmstedt <filip.almstedt@almxer.se>
2021-03-22 13:30:51 +0100

    Added error message to landing page in case the user hasn't logged in and wants to donate

--- Middleware/verifyUser.js
+++ Middleware/verifyUser.js
@@ -1,13 +1,20 @@
 const jwt = require("jsonwebtoken");
 const { secretKey } = require("../Config/config")
 
+let errors = [];
+
 const verifyToken = (req, res, next) => {
+
+  //reset the error message
+  errors = [];
+
   const token = req.cookies.jsonWebToken;
   if (!token) 
   {
 
+    errors.push("You are not logged in! To donate you have to have an account! Please log in and try again!")
     console.log("error");
-  return res.render("landing.ejs", { err: "Login failed" });
+    return res.render("landing.ejs", { errors: errors });
   
   }
   const validUser = jwt.verify(token, secretKey);
