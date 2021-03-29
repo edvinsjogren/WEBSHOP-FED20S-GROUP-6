4f71f621e5b6d7ea2ff426660f4b4c2e48073ada
edvinsjogren <edvin.sjogren@gmail.com>
2021-03-29 09:27:53 +0200

    fixed navbar bugs

--- Controllers/checkoutController.js
+++ Controllers/checkoutController.js
@@ -2,14 +2,18 @@
 
 const Project = require("../Models/project");
 const User = require("../Models/user");
-const nodemailer = require("nodemailer")
-const {stripeSecretKey, stripePublicKey, nodeMailerUser, nodeMailerPassword} = require("../Config/config");
-const stripe = require("stripe")(stripeSecretKey); 
+const nodemailer = require("nodemailer");
+const {
+  stripeSecretKey,
+  stripePublicKey,
+  nodeMailerUser,
+  nodeMailerPassword,
+} = require("../Config/config");
+const stripe = require("stripe")(stripeSecretKey);
 
 //render projects on checkout page/in shoppingcart
 const checkoutRender = async (req, res) => {
   const projects = await Project.find().populate("img");
-
 
   const user = await User.findOne({ _id: req.user.user._id });
 
@@ -26,7 +30,6 @@ const checkoutRender = async (req, res) => {
       },
     })
     .execPopulate();
-
 
   //save the list of projects added in donationsInCart
   let donationsInCart = user.donations.projects;
@@ -49,36 +52,33 @@ const checkoutRender = async (req, res) => {
   }
   //console.log(donationsInCart);
 
-
   //Check if the cart is empty
-  if(!donationsInCart || donationsInCart.length === 0) {
-    req.flash('emptyDonationCart', 'Your cart is empty! Please add a donation from a project first!')
-    return res.redirect("/checkout")
+  if (!donationsInCart || donationsInCart.length === 0) {
+    req.flash(
+      "emptyDonationCart",
+      "Your cart is empty! Please add a donation from a project first!"
+    );
+    return res.redirect("/checkout");
   }
 
   // Create a stripe session
   const session = await stripe.checkout.sessions.create({
-
-    success_url: 'http://localhost:8000/sucessfulDonation',
-    cancel_url:  'https://localhost:8000/projects',
-    payment_method_types: ['card'],
-    line_items: donationsInCart.map( donation => {
-
-        return {
-            name: donation.projectID.title, 
-            // The donationAmount was only in cents, *100 equals the price in dollars. So if the donationAmount is 10, the amount is 0.10*100 = 10 USD
-            amount:  donation.donationAmount *100 , 
-            quantity: 1, 
-            currency: "usd"
-        }
-
-    }), 
-    mode: 'payment',
-
-  })
+    success_url: "http://localhost:8000/sucessfulDonation",
+    cancel_url: "https://localhost:8000/projects",
+    payment_method_types: ["card"],
+    line_items: donationsInCart.map((donation) => {
+      return {
+        name: donation.projectID.title,
+        // The donationAmount was only in cents, *100 equals the price in dollars. So if the donationAmount is 10, the amount is 0.10*100 = 10 USD
+        amount: donation.donationAmount * 100,
+        quantity: 1,
+        currency: "usd",
+      };
+    }),
+    mode: "payment",
+  });
 
   console.log(session);
-
 
   //render the projects specific for the user, on checkout page
   res.render("checkout.ejs", {
@@ -87,10 +87,9 @@ const checkoutRender = async (req, res) => {
     user: user,
     donationsInCart: donationsInCart,
     sessionId: session.id,
-    stripePublicKey: stripePublicKey
+    stripePublicKey: stripePublicKey,
   });
 };
-
 
 // Transport to initiate nodemailer
 const transport = nodemailer.createTransport({
@@ -102,17 +101,15 @@ const transport = nodemailer.createTransport({
 });
 
 //After the user paid an email is send to the user where you can see the donations that has been made
-const sucessfulDonation = async (req,res) => {
+const sucessfulDonation = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.user._id });
 
-  const user = await User.findOne({_id: req.user.user._id});
-  
   //Send confirmation mail with a link, showing the donations and the projects the user made
   await transport.sendMail({
     from: nodeMailerUser,
     to: user.email,
     subject: "Your donation(s) has been made!",
-    html: 
-      `
+    html: `
         <h1>Thank you for your donation ${user.username}!</h1>
         <p>Thank you for your contribution to these projects!</p>
         <p>For more information regarding to the projects you have made, visit <a href="http://localhost:8000/donationsMade">this link</a></p>
@@ -122,30 +119,28 @@ const sucessfulDonation = async (req,res) => {
         <p>Best regards,</p>
         <p>The Other 99</p>
 
-      `
+      `,
   });
 
-  res.render("payment.ejs");
+  res.render("payment.ejs", { user: user });
+};
 
-}
-
-//A "receipt" on the donations the user made. 
-const renderDonationsMade = async (req,res) => {
-
-  const user = await User.findOne({_id: req.user.user._id});
+//A "receipt" on the donations the user made.
+const renderDonationsMade = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.user._id });
 
   await user
-  .populate({
-    path: "donations",
-    populate: {
-      path: "projects.projectID",
+    .populate({
+      path: "donations",
       populate: {
-        path: "img",
-        select: "path"
-      }
-    },
-  })
-  .execPopulate();
+        path: "projects.projectID",
+        populate: {
+          path: "img",
+          select: "path",
+        },
+      },
+    })
+    .execPopulate();
 
   let donationsInCart = user.donations.projects;
 
@@ -154,19 +149,18 @@ const renderDonationsMade = async (req,res) => {
     (item) => item.donationAmount
   );
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  //Total sum of the all the money that has been donated to the project 
+  //Total sum of the all the money that has been donated to the project
   const totalSumInCart = userCartItemPricessMap.reduce(reducer, 0);
 
   res.render("donatedProjects.ejs", {
-    user: user, 
+    user: user,
     donatedProjects: donationsInCart,
-    totalSumInCart: totalSumInCart
-  })
+    totalSumInCart: totalSumInCart,
+  });
 
   //clears the donationCart after the user has seen the purchase
   user.clearDonationCart();
-
-}
+};
 
 //create request to delete a specific donation in the cart
 const deleteSubmit = async (req, res) => {
@@ -199,7 +193,7 @@ const editRender = async (req, res) => {
     (project) => project._id == req.params.id
   );
 
-  res.render("checkoutEdit.ejs", { project: selectedDonation });
+  res.render("checkoutEdit.ejs", { project: selectedDonation, user: user });
 };
 
 //post the edited amount on the checkout page
@@ -212,15 +206,12 @@ const editSubmit = async (req, res) => {
       "error",
       "You haven't chosen an amount to donate! Please try again!"
     );
-    return res.redirect("/checkout")
+    return res.redirect("/checkout");
   }
 
   //await User.updateOne({_id: id}, {donationAmount: donationAmount});
-  user.editDonation(id, donationAmount)
-  req.flash(
-    "success",
-    "You've updated your donation, time to seal the deal!"
-  );
+  user.editDonation(id, donationAmount);
+  req.flash("success", "You've updated your donation, time to seal the deal!");
   res.redirect("/checkout");
 };
 
@@ -230,5 +221,5 @@ module.exports = {
   editRender,
   editSubmit,
   sucessfulDonation,
-  renderDonationsMade
+  renderDonationsMade,
 };
